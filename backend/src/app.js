5732e8b4e79fc0ddca3a5d621af8d7e871ae82cc
Chandan kumar <120443395+Chandan-kumar5@users.noreply.github.com>
2025-10-04 20:39:12 +0530

    cors problem fixing

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://reeltomeal.vercel.app",
     credentials: true
 }));
 app.use(cookieParser());
